1fce5a8ce56e3b64e9393163548b05c54215ab6f
jonathan-crepeau <jon.crepeau@gmail.com>
2022-11-07 20:40:37 -0800

    basic db test start server complete

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Router = require("./routes")
-
+const express = require('express');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 4001;
 
-app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-mongoose.connect('mongodb://localhost:27017/usersdb',
-  {
-    useNewUrlParser: true
-  }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+app.listen(PORT, () => {
+  console.log(`Application is listening on port ${PORT}.`);
 });
